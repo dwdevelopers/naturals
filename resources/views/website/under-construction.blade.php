30238d55627c9a7d5884de00ac087600dca8b3d6
seethalshankar8 <seethalsasi@dawsonwalsh.com>
2025-07-01 11:24:18 +0530

    under construction

---
+++ resources/views/website/under-construction.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Site Under Construction</title>
+    <style>
+        body {
+            background-color: #f8f8f8;
+            font-family: 'Segoe UI', sans-serif;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            text-align: center;
+        }
+        .message {
+            max-width: 500px;
+        }
+    </style>
+</head>
+<body>
+    <div class="message">
+          <img src="{{ asset('website/images/naturals-logo.png') }}" alt=""  style="width: 190px;"/>
+        <h1>We'll be back soon!</h1>
+        <p>Our site is currently undergoing scheduled maintenance.<br>Thank you for your patience.</p>
+    </div>
+</body>
+</html>
